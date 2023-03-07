0429f65a6bb82e29bf1f885bd12647608b5c5e85
Aaron Boner <L00167055@atu.ie>
2023-03-07 19:12:41 +0000

    view ticket template

---
+++ scripts/viewticket.js
@@ -0,0 +1,11 @@
+var qrcode = new QRious({
+    element: document.getElementById("qrcode"),
+    background: '#ffffff',
+    backgroundAlpha: 1,
+    foreground: '#5868bf',
+    foregroundAlpha: 1,
+    level: 'H',
+    padding: 0,
+    size: 128,
+    value: website
+  });
